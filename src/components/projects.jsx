eecfeec481be6d4c16788ceb9f5771e7a8dce566
Glitch (glitch-hello-react) <none>
2021-09-11 18:43:08 +0000

    🐂🦄 Checkpoint ./src/styles/styles.css:4100075/96 ./src/pages/home.jsx:4100075/160 ./src/components/projects.jsx:4100075/1837

---
+++ src/components/projects.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {Project} from "./components/project"
+
+export const Projects = ()  => {
+
+  const projects = ['hummingbird'];
+  
+  return (
+    <div className="projects">
+       {projects.map((project, i) => (
+        <div key={"project-" + i}>
+            <Project project={project}/>
+        </div>
+    </div>
+  ))})
+      };
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,16 @@
 import * as React from "react";
 import { Link } from "wouter";
+import { Projects } from "../components/projects";
 
 export default function Home() {
   return (
     <div className="page">
-      <h1>Hey! 👋🏽 I'm Zainab Ismail, a London-based poet and </h1>{" "}
+      <h1 className="hey">Hey! 👋🏽 I'm Zainab Ismail, a London-based poet and </h1>{" "}
       <div className="typewriter">
         <h1>software engineer.</h1>
       </div>
+      
+      <Projects/>
     </div>
   );
 }
