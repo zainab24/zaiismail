f65a6494eb9c5c33aed1fbd36081c5a6910511b2
Glitch (glitch-hello-react) <none>
2021-09-11 19:28:27 +0000

    💓🎱 Checkpoint ./src/components/projects.jsx:4100075/1516 ./src/components/project.jsx:4100075/911

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { Project } from "./project";
+import * as data from "../../data/projects.json";
 
 export const Projects = () => {
-  const projects = ["hummingbird"];
-
-  return (
-    <div className="projects">
-      {projects.map((project, i) => (
-        <Project project={project} />
+  
+  const { data } = data; 
+  
+  return( 
+    
+     Object.keys(data).map((item, i) => (
+    <div key={"block-" + i}>
+      <h1>
+        {data.name} {data.date}
+      </h1>
+      <img src={data.URL} alt={data.name} />
+      {data.lines.map((line, i) => (
+        <div key={"line-" + i}>{line}</div>
       ))}
     </div>
-  );
+  )));
 };
