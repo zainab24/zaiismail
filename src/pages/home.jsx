ef54cec9024c5057b47f35468e1d2ffc4cb06a4a
Glitch (glitch-hello-react) <none>
2021-09-11 18:31:03 +0000

    🚘✊ Checkpoint ./src/styles/styles.css:4100075/23314 ./src/pages/home.jsx:4100075/2921

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,12 +4,10 @@ import { Link } from "wouter";
 export default function Home() {
   return (
     <div className="page">
-      <h1>
-        Hey! 👋🏽 I'm Zainab Ismail, a poet and
-        <div className="typewriter"> 
-          <h1>software engineer</h1>        </div>
-        from London.
-      </h1>
+      <h1>Hey! 👋🏽 I'm Zainab Ismail, a London-based poet and </h1>{" "}
+      <div className="typewriter">
+        <h1>software engineer.</h1>
+      </div>
     </div>
   );
 }
