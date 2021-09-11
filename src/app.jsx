39525f3e87c0719409e3d7ecaca3a19dee160513
Glitch (glitch-hello-react) <none>
2021-09-11 16:31:52 +0000

    🏎👠 Checkpoint ./src/app.jsx:4100075/2161 ./src/styles/styles.css:4100075/3073

--- src/app.jsx
+++ src/app.jsx
@@ -28,26 +28,23 @@ export default function Home() {
       <Seo />
       <main role="main" className="wrapper">
         <div className="content">
+          
+          <div id="box">
+  <div class="river river--1"></div>
+  <div class="river river--2"></div>
+
+  <svg>
+    <filter id="turbulence" x="0" y="0">
+      <feTurbulence baseFrequency="0.02 0.01"></feTurbulence>
+      <feDisplacementMap scale="45" in="SourceGraphic"></feDisplacementMap>
+    </filter>
+  </svg>
+</div>
           {/* Router specifies which component to insert here as the main content */}
           <PageRouter />
         </div>
       </main>
-      {/* Footer links to Home and About, Link elements matched in router.jsx */}
-      <footer className="footer">
-        <div className="links">
-          <Link href="/">Home</Link>
-          <span className="divider">|</span>
-          <Link href="/about">About</Link>
-        </div>
-        <a
-          className="btn--remix"
-          target="_top"
-          href="https://glitch.com/edit/#!/remix/glitch-hello-react"
-        >
-          <img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
-          Remix on Glitch
-        </a>
-      </footer>
+    
     </Router>
   );
 }
