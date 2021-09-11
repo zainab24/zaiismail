0c260134ed3e8c4dc70317189716af955b9f079f
Glitch (glitch-hello-react) <none>
2021-09-11 23:25:08 +0000

    🍽🖖 Checkpoint ./src/pages/hummingbird.jsx:4100075/2123

--- src/pages/hummingbird.jsx
+++ src/pages/hummingbird.jsx
@@ -4,18 +4,17 @@ export default function Hummingbird() {
   return (
     <div className="page">
       <div className="project">
-      <h1>
-        <a href="http://hummingbird.zaiismail.net">hummingbird</a>
-      </h1>
+        <h1>
+          <a href="http://hummingbird.zaiismail.net">hummingbird</a>
+        </h1>
 
-
-    
         <p>
-          This was my first real foray into using React, experimenting with the {" "}
-          <a href="http://poetrydb.org">poetrydb API</a>.
+          This was my first real foray into creating a React web app,
+          experimenting with the <a href="http://poetrydb.org">poetrydb API</a>{" "}
+          and developing my responsive design skills.
         </p>
-        </div>
-     <a href="http://hummingbird.zaiismail.net">
+      </div>
+      <a href="http://hummingbird.zaiismail.net">
         <img
           src="https://cdn.glitch.com/d7f5d655-bedb-46ba-b8a6-d3a4253e0b8b%2Fhummingbird.jpeg?v=1631386834026"
           className="screenshot--portrait"
@@ -28,6 +27,5 @@ export default function Hummingbird() {
         />
       </a>
     </div>
-    
   );
 }
