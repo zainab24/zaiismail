8e8bd1994f1721839a8b1278a6808f7de40b327f
Glitch (glitch-hello-react) <none>
2021-09-11 23:48:09 +0000

    📒🌸 Checkpoint ./src/components/projects.jsx:4100075/58

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -5,7 +5,8 @@ import { Link } from "wouter";
 export const Projects = () => {
   return projects.map((item, i) => (
     <div className="project" key={"item" + i}>
-      <a href="http://hummingbird.zaiismail.net"><img src={item.thumbnail} alt={item.name} /></a>
+      <a href="http://hummingbird.zaiismail.net">
+     <img src={item.thumbnail} alt={item.name} /></a>
       <h2><a href="http://hummingbird.zaiismail.net">{item.name}</a></h2>
       <p>{item.desc}<br/>{item.date}<br/><Link href={item.name}>read more</Link></p>
     </div>
