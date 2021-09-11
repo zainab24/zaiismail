6a9cf15073ce37131cfa571dbacd6d9a4fc54200
Glitch (glitch-hello-react) <none>
2021-09-11 19:06:01 +0000

    🎖🤗 Checkpoint ./data/projects.json:4100075/5032 ./.glitch-assets:4100075/393 ./src/components/project.jsx:4100075/1162

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import projects from './data.json';
+import * as data from './projects.json';
 
 export const Project = (props) => {
 
   return (
-    <div className={`project ${props.project}`}>
-      
-    </div>
+          props.map((item, i) => (
+        <div key={"block-" + i}>
+          <img src={data.URL}""
+          {data.lines.map((line, i) => (
+            <div key={"line-" + i}>
+              {line}
+            </div>
+          ))}
+          <div className="poem-data">— <span>{item.title}</span> by {item.author}</div>
+        </div>
   );
 };
