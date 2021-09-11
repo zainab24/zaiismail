8cfa3a0791b991e58ccbec65923e8dadb6f119c0
Glitch (glitch-hello-react) <none>
2021-09-11 21:25:30 +0000

    🤘🎟 Checkpoint ./data/publications.js:4100075/2796 ./data/digital.js:4100075/299 ./data/print.js:4100075/1797

---
+++ data/digital.js
@@ -0,0 +1,17 @@
+const digital = [
+  {
+    title: "Two villanelles, ‘Otherworld’ and ‘Pipette’",
+    publication: "Fortnightly Review",
+    URL: "https://fortnightlyreview.co.uk/2016/10/two-villanelles",
+    date:
+      "1 October 2016",
+  },{
+    title: "",
+    publication: "Fortnightly Review",
+    URL: "https://fortnightlyreview.co.uk/2016/10/two-villanelles",
+    date:
+      "1 October 2016",
+  }
+];
+
+export default digital;
---
+++ data/print.js
@@ -0,0 +1,17 @@
+const print = [
+  {
+    title: "‘sharp relief’ and ‘trilateral roots’",
+    publication: "Wretched Strangers",
+    URL: "https://www.boilerhouse.press/wretched-strangers",
+    date:
+      "23 June 2018",
+  },{
+        title: "‘Grandmother",
+    publication: "Tears in the Fence",
+    URL: "https://tearsinthefence.com/",
+    date:
+      "Summer 2017 (issue 66)",
+  }
+];
+
+export default print;
