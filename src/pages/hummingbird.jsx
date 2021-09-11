ff9dac7d0bbf6d803226df1bbe4c3180ec5e4ced
Glitch (glitch-hello-react) <none>
2021-09-11 22:37:07 +0000

    📮📌 Checkpoint ./.glitch-assets:4100075/933 ./src/styles/styles.css:4100075/11537 ./src/pages/hummingbird.jsx:4100075/3026 ./data/projects.js:4100075/1454 ./index.html:4100075/1096

--- src/pages/hummingbird.jsx
+++ src/pages/hummingbird.jsx
@@ -3,9 +3,28 @@ import React from "react";
 export default function Hummingbird() {
   return (
     <div className="page">
-      <h1><a href="http://hummingbird.zaiismail.net">hummingbird</a></h1>
-      <a href="http://hummingbird.zaiismail.net"><img src="https://cdn.glitch.com/d7f5d655-bedb-46ba-b8a6-d3a4253e0b8b%2Fhummingbird.jpeg?v=1631386834026" className="screenshot--portrait"/></a>
-      <p>This was my first real foray using React, experimenting with the <a href="http://poetrydb.org">poetrydb API</a>.</p>
+      <h1>
+        <a href="http://hummingbird.zaiismail.net">hummingbird</a>
+      </h1>
+
+      <a href="http://hummingbird.zaiismail.net">
+        <img
+          src="https://cdn.glitch.com/d7f5d655-bedb-46ba-b8a6-d3a4253e0b8b%2Fhummingbird-landscape.png?v=1631399201623"
+          className="screenshot--landscape"
+        />
+      </a>
+      <a href="http://hummingbird.zaiismail.net">
+        <img
+          src="https://cdn.glitch.com/d7f5d655-bedb-46ba-b8a6-d3a4253e0b8b%2Fhummingbird.jpeg?v=1631386834026"
+          className="screenshot--portrait"
+        />
+      </a>
+      <div className="info">
+        <p>
+          This was my first real foray into using React, experimenting with the {" "}
+          <a href="http://poetrydb.org">poetrydb API</a>.
+        </p>
+      </div>
     </div>
   );
 }
