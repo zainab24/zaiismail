b2e551d77345bf20152e5836233ec51118c44d84
Glitch (glitch-hello-react) <none>
2021-09-12 08:08:21 +0000

    💸🌌 Checkpoint ./src/pages/hummingbird.jsx:4100075/167 ./src/components/projects.jsx:4100075/13 ./src/styles/styles.css:4100075/2

--- src/pages/hummingbird.jsx
+++ src/pages/hummingbird.jsx
@@ -5,7 +5,8 @@ export default function Hummingbird() {
     <div className="page">
       <div className="project">
         <h1>
-          <a href="http://hummingbird.zaiismail.net">hummingbird</a>
+          <a href="http://hummingbird.zaiismail.net" target="_blank"
+            rel="noreferrer">hummingbird</a>
         </h1>
 
         <p>
@@ -25,18 +26,14 @@ export default function Hummingbird() {
           , and developed my responsive design skills.
         </p>
       </div>
-      <a href="http://hummingbird.zaiismail.net">
         <img
           src="https://cdn.glitch.com/d7f5d655-bedb-46ba-b8a6-d3a4253e0b8b%2Fhummingbird.jpeg?v=1631386834026"
           className="screenshot--portrait"
         />
-      </a>
-      <a href="http://hummingbird.zaiismail.net">
         <img
           src="https://cdn.glitch.com/d7f5d655-bedb-46ba-b8a6-d3a4253e0b8b%2Fhummingbird-landscape.png?v=1631399201623"
           className="screenshot--landscape"
         />
-      </a>
     </div>
   );
 }
