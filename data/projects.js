82de9f29c10c6180af4df0f18ff985d5b3a23954
Glitch (zaiismail) <none>
2021-09-12 12:29:51 +0000

    Rewound to commit 6d5ad331d82d3d3f2cb155b26a8111e22fe0e275 + 0 edits

--- data/projects.js
+++ data/projects.js
@@ -3,7 +3,7 @@ const projects = [
     name: "hummingbird",
     desc: "drag-n-drop poetry maker",
     thumbnail:
-      "",
+      "https://cdn.glitch.com/d7f5d655-bedb-46ba-b8a6-d3a4253e0b8b%2Fandroid-chrome-512x512.png?v=1631402819619",
     date: "September 2021"
   }
 ];
