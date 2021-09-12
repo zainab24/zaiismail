7612cbbdc22affa0a069549cdc420ac08104bb22
zai-i <zainab@wonderbly.com>
2021-09-12 18:38:21 +0100

    favicon experiment 3

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
-import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), ViteFaviconsPlugin()],
+  plugins: [reactRefresh()],
   build: {
     outDir: "build"
   },
