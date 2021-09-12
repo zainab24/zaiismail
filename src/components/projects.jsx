f158c5b0ed4ed6cf8110bda2ed41021bdceaebbb
Glitch (glitch-hello-react) <none>
2021-09-12 07:32:31 +0000

    🚋🎰 Checkpoint ./src/styles/styles.css:4100075/15566 ./src/components/projects.jsx:4100075/76 ./src/pages/hummingbird.jsx:4100075/100 ./src/pages/home.jsx:4100075/6

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -5,9 +5,9 @@ import { Link } from "wouter";
 export const Projects = () => {
   return projects.map((item, i) => (
     <div className="project" key={"item" + i}>
-      <a href="http://hummingbird.zaiismail.net">
+      <a target="_blank" rel="noreferrer" href="http://hummingbird.zaiismail.net">
      <img src={item.thumbnail} alt={item.name} /></a>
-      <h2><a href="http://hummingbird.zaiismail.net">{item.name}</a></h2>
+      <h2><a href="http://hummingbird.zaiismail.net" target="_blank" rel="noreferrer">{item.name}</a></h2>
       <p>{item.desc}<br/>{item.date}<br/><Link href={item.name}>read more</Link></p>
     </div>
   ));
--- src/pages/hummingbird.jsx
+++ src/pages/hummingbird.jsx
@@ -11,16 +11,16 @@ export default function Hummingbird() {
         <p>
           This was my first real foray into creating a React web app,
           experimenting with the <a href="http://poetrydb.org">poetrydb</a> API
-          and <a href="https://github.com/peterh32/react-drag-drop-container">react-drag-drop-container</a>, and developing my responsive design skills.
+          and <a target="_blank" rel="noreferrer" href="https://github.com/peterh32/react-drag-drop-container">react-drag-drop-container</a>, and developing my responsive design skills.
         </p>
       </div>
-      <a href="http://hummingbird.zaiismail.net">
+      <a target="_blank" rel="noreferrer"href="http://hummingbird.zaiismail.net">
         <img
           src="https://cdn.glitch.com/d7f5d655-bedb-46ba-b8a6-d3a4253e0b8b%2Fhummingbird.jpeg?v=1631386834026"
           className="screenshot--portrait"
         />
       </a>
-      <a href="http://hummingbird.zaiismail.net">
+      <a target="_blank" rel="noreferrer" href="http://hummingbird.zaiismail.net">
         <img
           src="https://cdn.glitch.com/d7f5d655-bedb-46ba-b8a6-d3a4253e0b8b%2Fhummingbird-landscape.png?v=1631399201623"
           className="screenshot--landscape"
