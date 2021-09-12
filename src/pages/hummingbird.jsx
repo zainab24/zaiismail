269fbdb409013b1000e1c9a3c221d1ec1229ef4e
Glitch (glitch-hello-react) <none>
2021-09-12 07:44:37 +0000

    📠👧 Checkpoint ./src/styles/styles.css:4100075/1468 ./src/pages/hummingbird.jsx:4100075/65 ./src/components/projects.jsx:4100075/33

--- src/pages/hummingbird.jsx
+++ src/pages/hummingbird.jsx
@@ -14,13 +14,13 @@ export default function Hummingbird() {
           and <a target="_blank" rel="noreferrer" href="https://github.com/peterh32/react-drag-drop-container">react-drag-drop-container</a>, and developing my responsive design skills.
         </p>
       </div>
-      <a target="_blank" rel="noreferrer"href="http://hummingbird.zaiismail.net">
+      <a href="http://hummingbird.zaiismail.net">
         <img
           src="https://cdn.glitch.com/d7f5d655-bedb-46ba-b8a6-d3a4253e0b8b%2Fhummingbird.jpeg?v=1631386834026"
           className="screenshot--portrait"
         />
       </a>
-      <a target="_blank" rel="noreferrer" href="http://hummingbird.zaiismail.net">
+      <a href="http://hummingbird.zaiismail.net">
         <img
           src="https://cdn.glitch.com/d7f5d655-bedb-46ba-b8a6-d3a4253e0b8b%2Fhummingbird-landscape.png?v=1631399201623"
           className="screenshot--landscape"
