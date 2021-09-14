c2f814069c2989e76ac4798c31780c8e8593eea8
zai-i <zainab@wonderbly.com>
2021-09-14 09:08:32 +0100

    add insta link

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export default function Contact() {
   return <div className="page">
-    Feel free to check out my <a href="https://www.linkedin.com/in/zainabismail/" target="_blank" rel="noreferrer">linkedin</a> or email me at <a href="mailto:zainabismail024@gmail.com">zainabismail024@gmail.com</a>.
+    Feel free to check out my <a href="https://www.linkedin.com/in/zainabismail/" target="_blank" rel="noreferrer">linkedin</a>, <a href="https://www.instagram.com/zai._i" target="_blank" rel="noreferrer">instagram</a> or email me at <a href="mailto:zainabismail024@gmail.com">zainabismail024@gmail.com</a>.
   </div>;
 }
