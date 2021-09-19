030617f6fef20b37fd282e02df262a5ed13e6dfa
zai-i <zainab@wonderbly.com>
2021-09-19 06:49:47 +0100

    toggle test 3

--- src/components/dark-mode-toggle.jsx
+++ src/components/dark-mode-toggle.jsx
@@ -1,6 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import '../styles/dark-theme.css';
-import '../styles/light-theme.css';
 
 export const DarkModeToggle = () => {
 const [isDark, setIsDark] = useState(localStorage.getItem("theme") === "dark" ? true : false);
