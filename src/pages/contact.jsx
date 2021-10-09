5de895a909a9436797073ae5421f64d6f57ff6ee
zai-i <zainab@wonderbly.com>
2021-10-09 21:52:00 +0100

    testing

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return <section className="page-1">
     <p>Check out my <a href="https://www.linkedin.com/in/zainabismail/" target="_blank" rel="noreferrer">linkedin</a>, <a href="https://www.instagram.com/zai._i" target="_blank" rel="noreferrer">instagram</a> or email me at <a href="mailto:zainabismail024@gmail.com">zainabismail024@gmail.com</a>.</p>
     <h2>Send a quick note</h2>
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="contact" method="POST" netlify>
   <p>
     <label>name <input type="text" name="name" /></label>   
   </p>
