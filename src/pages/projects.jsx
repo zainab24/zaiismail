2892cb1b8c64c3eeffaa81685aa26a2c178fdc87
zai-i <zainab@wonderbly.com>
2021-11-03 18:23:43 +0000

    reword

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Publications() {
   return (
    <>
-   <p>Lately I’ve been seeking ways to explore all the things I love in little online spaces, such as by developing the React app I’ve called <a href="http://hummingbird.zaiismail.net">hummingbird</a>. Fascinated by the concept of fridge magnet poetry in driving inspiration, I wanted to see what is creatively possible when this is transported into the digital realm.</p>
+   <p>Lately I’ve been seeking ways to explore all the things I love in little online spaces, such as by developing the React app I’ve called <a href="http://hummingbird.zaiismail.net">hummingbird</a>. Fascinated by the way fridge magnet poetry can drive inspiration, I wanted to see what is creatively possible with this concept, done digitally.</p>
    </>
   );
 }
