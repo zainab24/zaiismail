3c385e39163249cce6d1d5ded59305be3908ebda
zai-i <zainab@wonderbly.com>
2021-11-18 20:39:09 +0000

    styling changes

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -7,7 +7,10 @@ export default function Publications() {
    
   <div className="page">
     <h2><Link href="projects">Projects</Link> <span className="pipe">|</span> <Link href="/">back</Link></h2>
+
+    <div className="box">
    <p>Lately I’ve been seeking ways to explore all the things I love from within little online spaces. I’ve developed a React app I’ve called <a href="http://hummingbird.zaiismail.net">hummingbird</a>. Fascinated by how randomness and fridge magnet poetry can drive inspiration, I wanted to see what is creatively possible with this concept, done digitally.</p>
+   </div>
    </div>
   );
 }
