dd46a686e875c44cad128334b55d397372505ee6
zai-i <zainab@wonderbly.com>
2021-11-19 17:52:08 +0000

    fixes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   <h1>Zainab Ismail</h1><h2><span className="projects"><Link href="projects">Projects</Link></span> <span className="pipe">|</span> <span className="publications"><Link href="publications">Publications</Link></span> <span className="pipe">|</span> <span className="writing"><Link href="writing">Writing</Link></span></h2>
   
   <img className="me" src="/images/portrait.png"/>
-  <p>Hey 👋🏽 I’m Zainab Ismail, a London-based software engineer and poet.</p>
+  <p className="intro">Hey, I’m Zainab Ismail, a London-based software engineer and poet.</p>
   <p><a href="mailto:zainabismail024@gmail.com">email</a> <span className="pipe">|</span> <a href="http://instagram.com/zai._i">instagram</a> <span className="pipe">|</span> <a href="https://www.linkedin.com/in/zainabismail">linkedin</a></p>
   </div>
   );
