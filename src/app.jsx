d171ccd76e416c40762e554b4f337ae7323442a4
zai-i <zainab@wonderbly.com>
2022-02-28 11:26:39 +0000

    new theme

--- src/app.jsx
+++ src/app.jsx
@@ -69,7 +69,6 @@ export default function Home() {
     <div className="landscape-nav">
      <h2><span className="home"><Link href="/">HOME</Link></span> <span className="projects"><Link href="projects">PROJECTS</Link></span>   <span className="publications"><Link href="publications">PUBLICATIONS</Link></span>   <span className="writing"><Link href="writing">WRITING</Link></span></h2>
      </div>
-
       <main role="main" className="wrapper">
 
   <div className="page">
