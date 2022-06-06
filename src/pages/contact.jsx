36152825466018c0398f00d160569fd0baad598d
zai-i <zainab@wonderbly.com>
2022-06-06 11:14:55 +0100

    try this out

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,18 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function Contact() {
-    const [success, setSuccess] = useState(false);
-    const onSubmit = async (event, setSuccess) => {
-        setSuccess(true);
-      };
-      
     return ( 
 
     <div className="form">
-<form name="contact" method="POST" data-netlify="true" onSubmit={e => onSubmit(e, setSuccess)}>
+<form name="contact" method="POST" data-netlify="true">
 
-<input type="hidden" name="form-name" value="contact" />
-  <p>{ success ? 'Thanks for your message!' : 'Send a quick note' }</p>
+<input type="hidden" name="form-name" value="contact" action="success" />
+  <p>Send a quick note</p>
   <p>
     <label><span>Name</span> <input type="text" name="name" /></label>
   </p>
