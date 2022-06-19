5cfcde1e1fadd6eed928c00b0f943ffdc912b3f2
zai-i <zainab@wonderbly.com>
2022-06-19 19:37:42 +0100

    add portrait

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,8 +4,8 @@ import Contact from "./contact";
 export default function Home() {
   return (
     <>
-  <div className="intro"><p>Hey, I’m Zainab Ismail, a London-based software engineer and poet.</p>
-  <p className="links"><a href="http://instagram.com/zai._i">instagram</a> <span className="pipe">|</span> <a href="https://www.linkedin.com/in/zainabismail">linkedin</a></p></div>
+  <div className="intro"><p className="center"><img src="/images/portrait.png" alt="me" className="me"/></p><p>Hey, I’m Zainab Ismail, a London-based software engineer and poet.</p>
+  <p className="center"><a href="http://instagram.com/zai._i">instagram</a> <span className="pipe">|</span> <a href="https://www.linkedin.com/in/zainabismail">linkedin</a></p></div>
   
 <Contact/>
 </>
