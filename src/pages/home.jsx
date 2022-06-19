e98a763cc9b1df8c9afd96eeba6688411ab8b622
zai-i <zainab@wonderbly.com>
2022-06-19 19:25:14 +0100

    centre links

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
   <div className="intro"><p>Hey, I’m Zainab Ismail, a London-based software engineer and poet.</p>
-  <p><a href="http://instagram.com/zai._i">instagram</a> <span className="pipe">|</span> <a href="https://www.linkedin.com/in/zainabismail">linkedin</a></p></div>
+  <p className="links"><a href="http://instagram.com/zai._i">instagram</a> <span className="pipe">|</span> <a href="https://www.linkedin.com/in/zainabismail">linkedin</a></p></div>
   
 <Contact/>
 </>
