95bae48e6b8f67017dd43ed568c51f06aa9a14dc
zai-i <zainab@wonderbly.com>
2022-06-20 19:09:25 +0100

    face colours

--- src/app.jsx
+++ src/app.jsx
@@ -38,7 +38,7 @@ export default class Home extends React.Component {
   componentDidMount() {
     setTimeout(() => {
       this.setTimePassed();
-    }, 4500);
+    }, 1000000);
   }
 
   setTimePassed() {
