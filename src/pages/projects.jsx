c878df1675483f07a4ef1d104a37cf6805fff92c
zai-i <zainab@wonderbly.com>
2022-06-20 10:30:29 +0100

    fix wording

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -5,7 +5,7 @@ export default function Publications() {
    <>
     <div className="box projects">
     <h2>online projects</h2>
-   <p>Lately I’ve been seeking ways to explore all the things I love from within little online spaces. I’ve developed a React app I’ve called <a href="http://hummingbird.zaiismail.net">Hummingbird</a>. Fascinated by how randomness and fridge magnet poetry can drive inspiration, I wanted to see what is creatively possible with this concept, done digitally.</p>
+   <p>Lately I’ve been seeking ways to explore all the things I love in little online spaces. I’ve developed a React app I’ve called <a href="http://hummingbird.zaiismail.net">Hummingbird</a>. Fascinated by how randomness and fridge magnet poetry can drive inspiration, I wanted to see what is creatively possible with this concept, done digitally.</p>
   
     <p>For Arabic learning, I have created <a href="http://tajweed.zaiismail.net">a reference tool for Tajweed</a> (correct pronunciation of the Arabic alphabet) and <a href="http://alifba.zaiismail.net">Alifba</a>, a drag-and-drop educational game for children.</p>
    </div>
