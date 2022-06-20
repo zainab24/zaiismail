f3adefe2ee609023652f086817dd70529ddb243f
zai-i <zainab@wonderbly.com>
2022-06-20 17:51:39 +0100

    dolls and speeds

--- src/components/splash.jsx
+++ src/components/splash.jsx
@@ -4,102 +4,103 @@ export default function Splash() {
   return (
     <div id="splash">
       <div id="dolls">
-        <div class="body" id="body1">
-          <div class="face">
-            <div class="fringe"></div>
-            <div class="closed-eye closed-eye1"></div>
-            <div class="closed-eye closed-eye2"></div>
-            <div class="rosycheek rosycheek1"></div>
-            <div class="rosycheek rosycheek2"></div>
-            <div class="lips">
-              <div class="separator"></div>
+          <div className="body" id="body1">
+
+            <div className="face">
+              <div className="fringe"></div>
+              <div className="closed-eye closed-eye1"></div>
+              <div className="closed-eye closed-eye2"></div>
+              <div className="rosycheek rosycheek1"></div>
+              <div className="rosycheek rosycheek2"></div>
+              <div className="lips">
+                <div className="separator"></div>
+              </div>
             </div>
-          </div>
-          <div class="line"></div>
-          <div class="flower">
-            <div class="petal petal1"></div>
-            <div class="petal petal2"></div>
-            <div class="petal petal3"></div>
-            <div class="petal petal4"></div>
-            <div class="petal petal5"></div>
-            <div class="petal petal6"></div>
-            <div class="petal petal7"></div>
-            <div class="petal petal8"></div>
+            <div className="line"></div>
+            <div className="flower">
+              <div className="petal petal1"></div>
+              <div className="petal petal2"></div>
+              <div className="petal petal3"></div>
+              <div className="petal petal4"></div>
+              <div className="petal petal5"></div>
+              <div className="petal petal6"></div>
+              <div className="petal petal7"></div>
+              <div className="petal petal8"></div>
+
           </div>
         </div>
 
-        <div class="body" id="body2">
-          <div class="face">
-            <div class="fringe"></div>
-            <div class="closed-eye closed-eye1"></div>
-            <div class="closed-eye closed-eye2"></div>
-            <div class="rosycheek rosycheek1"></div>
-            <div class="rosycheek rosycheek2"></div>
-            <div class="lips">
-              <div class="separator"></div>
+          <div className="body" id="body2">
+            <div className="face">
+              <div className="fringe"></div>
+              <div className="closed-eye closed-eye1"></div>
+              <div className="closed-eye closed-eye2"></div>
+              <div className="rosycheek rosycheek1"></div>
+              <div className="rosycheek rosycheek2"></div>
+              <div className="lips">
+                <div className="separator"></div>
+              </div>
             </div>
+            <div className="flower">
+              <div className="petal petal1"></div>
+              <div className="petal petal2"></div>
+              <div className="petal petal3"></div>
+              <div className="petal petal4"></div>
+              <div className="petal petal5"></div>
+              <div className="petal petal6"></div>
+              <div className="petal petal7"></div>
+              <div className="petal petal8"></div>
+            </div>
+            <div className="line"></div>
           </div>
-          <div class="flower">
-            <div class="petal petal1"></div>
-            <div class="petal petal2"></div>
-            <div class="petal petal3"></div>
-            <div class="petal petal4"></div>
-            <div class="petal petal5"></div>
-            <div class="petal petal6"></div>
-            <div class="petal petal7"></div>
-            <div class="petal petal8"></div>
-          </div>
-          <div class="line"></div>
-        </div>
 
-        <div class="body" id="body3">
-          <div class="face">
-            <div class="fringe"></div>
-            <div class="closed-eye closed-eye1"></div>
-            <div class="closed-eye closed-eye2"></div>
-            <div class="rosycheek rosycheek1"></div>
-            <div class="rosycheek rosycheek2"></div>
-            <div class="lips">
-              <div class="separator"></div>
+          <div className="body" id="body3">
+            <div className="face">
+              <div className="fringe"></div>
+              <div className="closed-eye closed-eye1"></div>
+              <div className="closed-eye closed-eye2"></div>
+              <div className="rosycheek rosycheek1"></div>
+              <div className="rosycheek rosycheek2"></div>
+              <div className="lips">
+                <div className="separator"></div>
+              </div>
+            </div>
+            <div className="line"></div>
+            <div className="flower">
+              <div className="petal petal1"></div>
+              <div className="petal petal2"></div>
+              <div className="petal petal3"></div>
+              <div className="petal petal4"></div>
+              <div className="petal petal5"></div>
+              <div className="petal petal6"></div>
+              <div className="petal petal7"></div>
+              <div className="petal petal8"></div>
             </div>
           </div>
-          <div class="line"></div>
-          <div class="flower">
-            <div class="petal petal1"></div>
-            <div class="petal petal2"></div>
-            <div class="petal petal3"></div>
-            <div class="petal petal4"></div>
-            <div class="petal petal5"></div>
-            <div class="petal petal6"></div>
-            <div class="petal petal7"></div>
-            <div class="petal petal8"></div>
-          </div>
-        </div>
-
-        <div class="body" id="body4">
-          <div class="face">
-            <div class="fringe"></div>
-            <div class="closed-eye closed-eye1"></div>
-            <div class="closed-eye closed-eye2"></div>
-            <div class="rosycheek rosycheek1"></div>
-            <div class="rosycheek rosycheek2"></div>
-            <div class="lips">
-              <div class="separator"></div>
+          <div className="body" id="body4">
+            <div className="face">
+              <div className="fringe"></div>
+              <div className="closed-eye closed-eye1"></div>
+              <div className="closed-eye closed-eye2"></div>
+              <div className="rosycheek rosycheek1"></div>
+              <div className="rosycheek rosycheek2"></div>
+              <div className="lips">
+                <div className="separator"></div>
+              </div>
             </div>
-          </div>
-          <div class="line"></div>
-          <div class="flower">
-            <div class="petal petal1"></div>
-            <div class="petal petal2"></div>
-            <div class="petal petal3"></div>
-            <div class="petal petal4"></div>
-            <div class="petal petal5"></div>
-            <div class="petal petal6"></div>
-            <div class="petal petal7"></div>
-            <div class="petal petal8"></div>
+            <div className="line"></div>
+            <div className="flower">
+              <div className="petal petal1"></div>
+              <div className="petal petal2"></div>
+              <div className="petal petal3"></div>
+              <div className="petal petal4"></div>
+              <div className="petal petal5"></div>
+              <div className="petal petal6"></div>
+              <div className="petal petal7"></div>
+              <div className="petal petal8"></div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
